8e4757b892f231690b63975fd0ba5714df6388ed
PLISSON Clement <clement.plisson@telecomnancy.eu>
2024-10-14 20:06:38 +0200

    code plus propre avec les pages qui gere elle meme les clic souris et non la fonction run

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -1,4 +1,4 @@
-#include "game.h"
+#include "TicTacToe.h"
 #include <SFML/Graphics.hpp>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,8 +10,28 @@
 using namespace sf;
 using namespace std;
 
-void Game::init(Player player) {
-    return_match();
+void TicTacToe::init_rematch(){
+    this->turn = Player::X;
+    this->tour = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->map[i][j] = Player::None;
+        }
+    }
+}
+
+void TicTacToe::init(){
+    if (!this->font.loadFromFile("./font/MegamaxJonathanToo-YqOq2.ttf")) {
+        std::cerr << "Erreur de chargement de la police." << std::endl;
+    }
+    this->window = GameWindow::Menu;
+}
+
+void TicTacToe::init_socket(Player player) {
+    init_rematch();
+
+    this->player = player;
 
     if (player == Player::X) {//Server
         TcpListener server;
@@ -44,30 +64,20 @@ void Game::init(Player player) {
     }
 }
 
-void Game::play(int i, int j, Player player){
+void TicTacToe::play(int i, int j, Player player){
     if (this->map[i][j] == Player::None && player == this->turn && 0 <= i && i <= 3 && 0 <= j && j <= 3){
         this->map[i][j] = player;
         this->tour++;
         this->turn = player == Player::O ? Player::X : Player::O;
+        if (!is_ongoing()) this->window = GameWindow::End;
     }
 }
 
-bool Game::is_ongoing(){
+bool TicTacToe::is_ongoing(){
     return winner() == Player::None && this->tour < 9;
 }
 
-void Game::return_match(){
-    this->turn = Player::X;
-    this->tour = 0;
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            this->map[i][j] = Player::None;
-        }
-    }
-}
-
-Player Game::winner(){
+Player TicTacToe::winner(){
     if (tour < 5) return Player::None;
 
     for(int i =0;i<3;i++){
@@ -83,7 +93,7 @@ Player Game::winner(){
     return Player::None;
 }
 
-void Game::draw(RenderWindow* window){
+void TicTacToe::draw(RenderWindow* window){
     Vertex line1[] = {
         Vertex(Vector2f(WINDOW/3+1, 0)),
         Vertex(Vector2f(WINDOW/3+1, WINDOW+2))
@@ -137,23 +147,15 @@ void Game::draw(RenderWindow* window){
     }
 }
 
-void Game::accept_return_match(RenderWindow* window){
+void TicTacToe::accept_rematch(RenderWindow* window){
     RectangleShape background(Vector2f(WINDOW, WINDOW));
     background.setFillColor(Color(0, 0, 0, 192));
     window->draw(background);
 
-    static Font font; 
-    static bool fontLoaded = false;
-    if (!fontLoaded) {
-        if (!font.loadFromFile("./font/MegamaxJonathanToo-YqOq2.ttf")) {
-            std::cerr << "Erreur de chargement de la police." << std::endl;
-        }
-        fontLoaded = true;
-    }
     Text menu;
     menu.setFont(font); 
     menu.setString("Menu");
-    menu.setCharacterSize(72);
+    menu.setCharacterSize(48);
     menu.setFillColor(Color::White);
     menu.setPosition((WINDOW +2 - menu.getGlobalBounds().width) / 2, WINDOW /4+26);
     window->draw(menu);
@@ -169,7 +171,7 @@ void Game::accept_return_match(RenderWindow* window){
     Text yes;
     yes.setFont(font); 
     yes.setString("YES");
-    yes.setCharacterSize(72);
+    yes.setCharacterSize(48);
     yes.setFillColor(Color::White);
     yes.setPosition(WINDOW / 3, 3*WINDOW / 4);
     window->draw(yes);
@@ -177,144 +179,185 @@ void Game::accept_return_match(RenderWindow* window){
     Text no;
     no.setFont(font); 
     no.setString("NO");
-    no.setCharacterSize(72);
+    no.setCharacterSize(48);
     no.setFillColor(Color::White);
     no.setPosition(2*WINDOW / 3, 3*WINDOW / 4);
     window->draw(no);
+    
+    if (Mouse::isButtonPressed(Mouse::Left)){
+        Vector2i pos = Mouse::getPosition(*window);
+
+        RectangleShape menu(Vector2f(206,46));
+        menu.setPosition(199,204);
+        
+        RectangleShape yes(Vector2f(100,47));
+        yes.setPosition(WINDOW / 3, 3*WINDOW / 4);
+
+        RectangleShape no(Vector2f(75,47));
+        no.setPosition(2*WINDOW / 3, 3*WINDOW / 4);
+
+        if (menu.getGlobalBounds().contains(pos.x,pos.y)) window->close();
+
+        if (yes.getGlobalBounds().contains(pos.x,pos.y)) {
+            init_rematch();
+            this->window = GameWindow::Play;    
+        }
+
+        if (no.getGlobalBounds().contains(pos.x,pos.y)) {
+            this->window = GameWindow::Menu;           
+        }
+    }
 }
 
-void Game::menu(RenderWindow* window) {
+void TicTacToe::end(RenderWindow* window) {
     RectangleShape background(Vector2f(WINDOW, WINDOW));
     background.setFillColor(Color(0, 0, 0, 192));
     window->draw(background);
 
-    static Font font; 
-    static bool fontLoaded = false;
-    if (!fontLoaded) {
-        if (!font.loadFromFile("./font/MegamaxJonathanToo-YqOq2.ttf")) {
-            std::cerr << "Erreur de chargement de la police." << std::endl;
-        }
-        fontLoaded = true;
-    }
-
     Text menu;
-    menu.setFont(font); 
+    menu.setFont(this->font); 
     menu.setString("Menu");
-    menu.setCharacterSize(72);
+    menu.setCharacterSize(48);
     menu.setFillColor(Color::White);
     menu.setPosition((WINDOW +2 - menu.getGlobalBounds().width) / 2, WINDOW /4+26);
     window->draw(menu);
 
-    Text text;
-    text.setFont(font); 
-    text.setString("New Game");
-    text.setCharacterSize(72);
-    text.setFillColor(Color::White);
-    text.setPosition((WINDOW +2- text.getGlobalBounds().width) / 2, 2*WINDOW / 4+26);
-    window->draw(text);
+    Text rematch;
+    rematch.setFont(this->font); 
+    rematch.setString("Rematch");
+    rematch.setCharacterSize(48);
+    rematch.setFillColor(Color::White);
+    rematch.setPosition((WINDOW +2- rematch.getGlobalBounds().width) / 2, 2*WINDOW / 4+26);
+    window->draw(rematch);
+    
+    
+    if (Mouse::isButtonPressed(Mouse::Left)){
+        Vector2i pos = Mouse::getPosition(*window);
+
+        if (menu.getGlobalBounds().contains(pos.x,pos.y)) this->window = GameWindow::Menu;
+        if (rematch.getGlobalBounds().contains(pos.x,pos.y)) {
+            Move move = {-1,-1};
+            opponent.send(&move,sizeof(Move));
+            this->window = GameWindow::Play;
+            init_rematch();                 
+        }
+    }
+    
 }
 
-void Game::run(RenderWindow* window, Player player){
-    init(player);
+void TicTacToe::menu(RenderWindow* window){
+
+    Text text;
+    text.setFont(this->font); 
+    text.setString("TicTacToe");
+    text.setCharacterSize(72);
+    text.setFillColor(Color::White);
+    text.setPosition((WINDOW +2 - text.getGlobalBounds().width) / 2, WINDOW /4-36);
+    window->draw(text);
+
+    Text create;
+    create.setFont(this->font); 
+    create.setString("Create Game");
+    create.setCharacterSize(48);
+    create.setFillColor(Color::White);
+    create.setPosition((WINDOW +2- create.getGlobalBounds().width) / 2, 2*WINDOW / 4);
+    window->draw(create);
+
+    Text join;
+    join.setFont(this->font); 
+    join.setString("Join Game");
+    join.setCharacterSize(48);
+    join.setFillColor(Color::White);
+    join.setPosition((WINDOW +2- join.getGlobalBounds().width) / 2, 3*WINDOW / 4);
+    window->draw(join);
+
+    if (Mouse::isButtonPressed(Mouse::Left)){
+        Vector2i pos = Mouse::getPosition(*window);
+        if (join.getGlobalBounds().contains(pos.x,pos.y)) {
+            init_socket(Player::O); 
+            this->window = GameWindow::Play;
+        }
+        if (create.getGlobalBounds().contains(pos.x,pos.y)){
+            init_socket(Player::X);
+            this->window = GameWindow::Play;
+        }
+    }
+
+}
+
+
+void TicTacToe::run(RenderWindow* window){
     Event event;
-    bool rematch = false;
+    size_t received = sizeof(Move);
+    init();
 
     while (window->isOpen()) {
+        
         while (window->pollEvent(event)) {
             if (event.type == Event::Closed) {
                 window->close();
                 //send quit
             }
         }
-        
+
         window->clear();
+        switch (this->window)
+        {
+        case GameWindow::Menu:
+            menu(window);
+            break;
 
-        if (!is_ongoing()){
+        case GameWindow::Credit:
+            //
+            break;
 
-            if (!rematch){
-                draw(window);
-                menu(window);
-
-                if (Mouse::isButtonPressed(Mouse::Left)){
-                    Vector2i pos = Mouse::getPosition(*window);
-
-                    RectangleShape menu(Vector2f(206,46));
-                    menu.setPosition(199,204);
-                    
-                    RectangleShape new_game(Vector2f(416,47));
-                    new_game.setPosition(94,353);
-
-                    if (menu.getGlobalBounds().contains(pos.x,pos.y)) window->close();
-
-                    if (new_game.getGlobalBounds().contains(pos.x,pos.y)) {
-                        Move move = {-1,-1};
-                        opponent.send(&move,sizeof(Move));
-                        return_match();                 
-                    }
-                }
-                Move move;
-                size_t received = sizeof(Move);
-                opponent.receive(&move,sizeof(Move),received);
-                if (move.i == -1 && received == sizeof(Move)) {
-                    rematch = true;
-                    cout << "rematch ?" << endl;
-                }
-
-            }
-            else {
-                draw(window);
-                accept_return_match(window);
-
-                if (Mouse::isButtonPressed(Mouse::Left)){
-                    Vector2i pos = Mouse::getPosition(*window);
-
-                    RectangleShape menu(Vector2f(206,46));
-                    menu.setPosition(199,204);
-                    
-                    RectangleShape yes(Vector2f(50,47));
-                    yes.setPosition(WINDOW / 3, 3*WINDOW / 4);
-
-                    RectangleShape no(Vector2f(50,47));
-                    no.setPosition(2*WINDOW / 3, 3*WINDOW / 4);
-
-                    if (menu.getGlobalBounds().contains(pos.x,pos.y)) window->close();
-
-                    if (yes.getGlobalBounds().contains(pos.x,pos.y)) {
-                        return_match();
-                        rematch = false;                 
-                    }
-
-                    if (no.getGlobalBounds().contains(pos.x,pos.y)) {
-                        //      rematch = false;             
-                    }
-                }
-
-
-            }
-        }  
-
-        else if (player == this->turn){
-            if (Mouse::isButtonPressed(Mouse::Right)){
-                Vector2i pos = Mouse::getPosition(*window);
-                Vector2i case_pos = Vector2i(pos.x / (WINDOW/3),pos.y / (WINDOW/3));
-                Move move = {case_pos.x,case_pos.y};
-                opponent.send(&move,sizeof(Move));
-                play(case_pos.x,case_pos.y,player);
-            }
+        case GameWindow::End:
             draw(window);
-        } 
-        
-        else  {
-            draw(window);
-            window->display();
+            end(window);
 
             Move move;
-            size_t received = sizeof(Move);
             opponent.receive(&move,sizeof(Move),received);
-            if (received == sizeof(Move)) play(move.i, move.j, player == Player::O ? Player::X : Player::O);
-            
-        }
+            if (move.i == -1 && received == sizeof(Move)) {
+                this->window = GameWindow::Rematch;
+                cout << "rematch ?" << endl;
+            }
+        
+            break;
+        
+        case GameWindow::Play:
+            if (this->player == this->turn){
+                if (Mouse::isButtonPressed(Mouse::Right)){
+                    Vector2i pos = Mouse::getPosition(*window);
+                    Vector2i case_pos = Vector2i(pos.x / (WINDOW/3),pos.y / (WINDOW/3));
+                    Move move = {case_pos.x,case_pos.y};
+                    opponent.send(&move,sizeof(Move));
+                    play(case_pos.x,case_pos.y,this->player);
+                }
+                
+            } 
+            else  {
+                Move move;
+                opponent.receive(&move,sizeof(Move),received);
+                if (received == sizeof(Move)) play(move.i, move.j, this->player == Player::O ? Player::X : Player::O);
+                
+            }
+            draw(window);
+            break;
 
+        case GameWindow::Rematch:
+            draw(window);
+            accept_rematch(window);
+            break;
+
+        }
         window->display();
     }
+}
+
+int main() {
+    RenderWindow window(VideoMode(WINDOW + 2, WINDOW + 2), "TicTacToe", Style::Close);
+    TicTacToe game;
+    game.run(&window);
+
+    return 0;
 }
--- src/TicTacToe.h
+++ src/TicTacToe.h
@@ -3,27 +3,34 @@
 using namespace sf;
 
 int const WINDOW = 600;
-const int PORT = 11972;
+const int PORT = 11542;
 
 enum class Player { O,X,None };
+enum class GameWindow { Menu,Credit,Play,Rematch,End };
 
-class Game{
+class TicTacToe{
     private : 
         int tour;
         Player map[3][3];
+        Player player;
         Player turn;
         TcpSocket opponent;
-        void return_match();
-        void init(Player player);
+        GameWindow window;
+        Font font;
+
+        void init_rematch();
+        void init();
+        void init_socket(Player player);
         Player winner();
         void draw(RenderWindow* window);
         void play(int i,int j,Player player);
         bool is_ongoing();
+        void end(RenderWindow* window);
+        void accept_rematch(RenderWindow* window);
         void menu(RenderWindow* window);
-        void accept_return_match(RenderWindow* window);
 
     public:
-        void run(RenderWindow* window,Player player);
+        void run(RenderWindow* window);
 };
 
 struct Move { int i,j;};
